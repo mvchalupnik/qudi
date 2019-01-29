c93151437cc75266965272d74452c5782b64f90f
Roger <b16lukin@gmail.com>
2019-01-29 11:42:59 -0500

    Completed revising tektronix_awg7k driver

--- hardware/awg/tektronix_awg7k.py
+++ hardware/awg/tektronix_awg7k.py
@@ -20,6 +20,8 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 """
 
+# TODO: implement Dynamic Jump sequence mode
+# TODO: implement (write_)sub-sequence mode
 
 import os
 import time
